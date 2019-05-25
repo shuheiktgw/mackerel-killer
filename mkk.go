29644aaf9ac2dffe7abb4c1e4f212011bbe383fc
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2019-05-25 14:31:04 +0900

    Seperate Mkk.Apply into Mkk.Apply and Mkk.Kill

--- mkk.go
+++ mkk.go
@@ -1,7 +1,6 @@
 package mkk
 
 import (
-	"log"
 	"time"
 
 	"github.com/mackerelio/mackerel-client-go"
@@ -12,47 +11,35 @@ type Mkk struct {
 	Client *mackerel.Client
 }
 
-type Options struct {
-	DryRun bool
-}
-
 func NewMkk(token string) *Mkk {
 	return &Mkk{Client: mackerel.NewClient(token)}
 }
 
-func (m *Mkk) Kill(params *mackerel.FindHostsParam, filters []Filter, options *Options) ([]*mackerel.Host, error) {
+func (m *Mkk) Apply(params *mackerel.FindHostsParam, filters []Filter) ([]*mackerel.Host, error) {
 	hosts, err := m.Client.FindHosts(params)
 	if err != nil {
-		return nil, errors.Wrap(err, "Mkk.Kill fails while finding hosts")
+		return nil, errors.Wrap(err, "Mkk.Apply fails while finding hosts")
 	}
 
 	for _, f := range filters {
 		hosts, err = f.Apply(m.Client, hosts)
 		if err != nil {
-			return nil, errors.Wrap(err, "Mkk.Kill fails while applying filters")
+			return nil, errors.Wrap(err, "Mkk.Apply fails while applying filters")
 		}
-	}
-
-	if options.DryRun {
-		log.Println("mackerel-killer is under Dry Run mode")
-
-		for _, host := range hosts {
-			log.Printf("ID: %v, Name: %v, Roles: %v will be retired\n", host.ID, host.Name, host.Roles)
-		}
-
-		return hosts, nil
-	}
-
-	for _, host := range hosts {
-		time.Sleep(2 * time.Millisecond)
-
-		err = m.Client.RetireHost(host.ID)
-		if err != nil {
-			return nil, errors.Wrap(err, "Mkk.Kill fails while retiring hosts")
-		}
-
-		log.Printf("Retired ID: %v, Name: %v, Roles: %v\n", host.ID, host.Name, host.Roles)
 	}
 
 	return hosts, nil
+}
+
+func (m *Mkk) Kill(hosts []*mackerel.Host) error {
+	for _, host := range hosts {
+		time.Sleep(2 * time.Millisecond)
+
+		err := m.Client.RetireHost(host.ID)
+		if err != nil {
+			return errors.Wrap(err, "Mkk.Kill fails while retiring hosts")
+		}
+	}
+
+	return nil
 }
